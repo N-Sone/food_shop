a542fb3aaae6aea55ad73121570922ff411f666c
Nkumbe Nkengsone Ekwembwe <nkumbe.ekwembwe@stud.uni-due.de>
2022-09-08 01:42:52 +0200

    initial App setup

---
+++ src/main/java/com/lob/ndeeddelivery/NdeeDoApplication.java
@@ -0,0 +1,13 @@
+package com.lob.ndeeddelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NdeeDoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NdeeDoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lob/ndeeddelivery/NdeeDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lob.ndeeddelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NdeeDoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
